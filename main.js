027417edd50690bc7b8cd091c988f7ddf1d3b4ce
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-08-13 18:32:59 +0200

    Init work

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+import config from "@proxtx/config";
 
+export class Component {
+  constructor(service, config) {
+    this.service = service;
+    this.config = config;
+  }
+
+  functions = {
+    pull: async () => {},
+  };
+
+  getData = () => {
+    return {
+      host: config.smConfig.host,
+      file: this.config.file,
+      path: this.service.config.path,
+    };
+  };
+
+  mainWidgets = ["main-widget"];
+
+  configConfig = [
+    {
+      name: "file",
+      type: "text",
+      value: "config.json",
+    },
+  ];
+}
