ce51ee796653b1babc22e01fd3cf3670dcbf644e
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-08-16 15:58:13 +0200

    Add git loop

--- main.js
+++ main.js
@@ -2,10 +2,23 @@ import git from "simple-git";
 import { exec } from "child_process";
 
 export class Component {
+  loopEnabled = true;
+
   constructor(service, config) {
     this.service = service;
     this.config = config;
     this.git = new git(this.service.config.path);
+    if (this.config.loop) {
+      this.gitLoop();
+    }
+  }
+
+  gitLoop() {
+    if (!this.loopEnabled) return;
+    this.functions.pull();
+    setTimeout(() => {
+      this.gitLoop();
+    }, 1000 * 60 * 5);
   }
 
   functions = {
@@ -23,6 +36,10 @@ export class Component {
     return {};
   };
 
+  unload = () => {
+    this.loopEnabled = false;
+  };
+
   mainWidgets = ["main-widget"];
 
   configConfig = [
@@ -30,6 +47,11 @@ export class Component {
       name: "additionalCommand",
       type: "text",
       value: "npm i",
+    },
+    {
+      name: "loop",
+      type: "bool",
+      value: "true",
     },
   ];
 }
